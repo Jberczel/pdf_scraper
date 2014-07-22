ea0b7a8dfb5e3c6016842454aed3c7dd5fae6a75
Jamie <jxberc@gmail.com>
2014-07-22 08:17:03 -0400

    finish cjp_scraper

--- cjp_scraper.rb
+++ cjp_scraper.rb
@@ -2,44 +2,46 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-
-PAGE_URL = "http://psroc.phys.ntu.edu.tw/cjp/issues.php?vol=52&num=2"
+HOME_URL = "http://psroc.phys.ntu.edu.tw/cjp/issues.php"
+BASE_DIR = 'downloads/'
 BASE_URL = "http://psroc.phys.ntu.edu.tw/cjp/"
 
-FILE_DIR = "cjp_files"
+# get links to issues on homepage
+issues = Nokogiri::HTML(open(HOME_URL)).css('#main_container a')
 
-# create directory for downloaded files
-Dir.mkdir(FILE_DIR) unless File.exists?(FILE_DIR)
+# get first 3 links(issues)
+issues[0..2].each do |issue| 
 
-# create Nokogiri object from page_url; used to scrape web page by css tags
-page = Nokogiri::HTML(open(PAGE_URL))
+    # url to specific issue
+    page_url = HOME_URL + issue['href'] 
 
-# get links by <a> tag, and then filter by .pdf extension
-#links = page.css('a').select { |l| l['href'].end_with? FILE_TYPE if l['href'] }
+    # create Nokogiri object from page_url; used to scrape web page by css tags
+    page = Nokogiri::HTML(open(page_url))
 
-links = page.css('.td_paper')
+    # create directory for each issue
+    issue_name =  page.css('.oneLine_align_left').text
+    file_dir = BASE_DIR + issue_name
+    Dir.mkdir(file_dir) unless File.exists?(file_dir)
 
+    # get list of papers in issue
+    papers = page.css('.td_paper') 
 
-puts "There are #{links.length} files."
-i = 1
-links.each do |link|
-  title = link.css('.paper_title').text[0..30]
-  href = link.css('a')[0]['href']
+    puts "There are #{papers.length} papers in #{issue_name}:"
 
- puts title.encoding
-
-  puts "Fetching...file #{i}: #{title}"
-  local_fname = "#{FILE_DIR}/#{title}.pdf"
-
-  remote_data = open(BASE_URL + href).read
-  f = File.open local_fname, 'w'
-  f.write remote_data
-  f.close
-  puts "\t...Success, saved as #{local_fname}"
-  i += 1
-  sleep 1
-
-
+    #######################################
+    # download and save papers to directory
+    #######################################
+    papers.each_with_index do |paper, i|
+        title = paper.css('.paper_title').text[0..30]
+        href = paper.css('a')[0]['href']
+        puts "Fetching...file #{i+1}: #{title}"
+        local_fname = "#{file_dir}/#{title}.pdf"
+        remote_data = open(BASE_URL + href).read
+        f = File.open local_fname, 'w'
+        f.write remote_data
+        f.close
+        puts "\t...Success, saved as #{local_fname}"
+        sleep 1 
+    end
+    puts "Downloads Complete (#{issue_name})\n\n"
 end
-
-puts "Downloads Complete"
